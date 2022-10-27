a45a09b265da7a594536d6efb9439d6e52c384b9
HarukaIonaSao <robertaribeiro004@gmail.com>
2022-10-27 12:41:25 -0300

    :rocket:Criação do projeto em React e Home css e tsx

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+    <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://68.media.tumblr.com/3f3986ec1e94210dd9de27945a7f0bde/tumblr_ondkf5LDE41qbw2q1o1_1280.gif " alt="Gif futurista predominantemente roxa com naves passando de um lado para outro " className = "img"/>
+    </>
+    );
+}
+
+export default Home;
